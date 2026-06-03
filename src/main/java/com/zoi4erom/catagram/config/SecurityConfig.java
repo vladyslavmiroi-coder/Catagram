cc930bea45f8f5d4a87a9cbd4259077b55884aa7
Javystik <miroivladik@gmail.com>
2026-06-03 09:01:26 +0300

    Deploy

--- src/main/java/com/zoi4erom/catagram/config/SecurityConfig.java
+++ src/main/java/com/zoi4erom/catagram/config/SecurityConfig.java
@@ -52,9 +52,7 @@ public class SecurityConfig {
 
                 CorsConfiguration config = new CorsConfiguration();
 
-                config.setAllowedOrigins(List.of(
-                        "http://localhost:5173"
-                ));
+                config.setAllowedOriginPatterns(List.of("*"));
 
                 config.setAllowedMethods(List.of(
                         "GET",
